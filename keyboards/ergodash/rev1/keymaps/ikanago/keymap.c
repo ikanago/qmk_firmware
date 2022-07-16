310753fd098ee115b082f338bd2c4205b5019c6b
ikanago <ikanago-dev@protonmail.com>
2022-07-16 13:33:03 +0900

    Enable tap dance
    For 'q', send q for single tap and ESC for double tap.

--- keyboards/ergodash/rev1/keymaps/ikanago/keymap.c
+++ keyboards/ergodash/rev1/keymaps/ikanago/keymap.c
@@ -7,21 +7,34 @@
 #define _RAISE 2
 #define _DRAW 3
 #define _ADJUST 16
-
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-  REVERT,
-  ZOOMIN,
-  ZOOMOUT,
-};
-
 #define EISU LALT(KC_GRV)
 #define W_PSCR LGUI(S(KC_S))
 
+enum custom_keycodes {
+    QWERTY = SAFE_RANGE,
+    LOWER,
+    RAISE,
+    ADJUST,
+    REVERT,
+    ZOOMIN,
+    ZOOMOUT,
+};
+
+#ifdef TAP_DANCE_ENABLE
+#    include "process_tap_dance.h"
+
+enum tap_dance_codes {
+    TD_Q_ESC = 0,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
+};
+#endif  // TAP_DANCE_ENABLE
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    // clang-format off
 
   /* Qwerty
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -37,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    JP_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC ,                        KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        JP_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_LBRC,                        JP_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TG(_DRAW),                      KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_BSPC,KC_DEL,          KC_ENT,KC_SPC , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    JP_GRV,  KC_1,            KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC ,                        KC_BSPC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR, \
+    KC_TAB,  TD(TD_Q_ESC),    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        JP_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_BSLS, \
+    KC_LCTL, KC_A,            KC_S,    KC_D,    KC_F,    KC_G,    JP_LBRC,                        JP_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_QUOT, \
+    KC_LSFT, KC_Z,            KC_X,    KC_C,    KC_V,    KC_B,    TG(_DRAW),                      KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LCTL, KC_LGUI,         KC_LALT, EISU,             LOWER,   KC_BSPC,KC_DEL,          KC_ENT,KC_SPC , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower
@@ -129,63 +142,65 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+// clang-format on
+
 void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
+    eeconfig_update_default_layer(default_layer);
+    default_layer_set(default_layer);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        print("mode just switched to qwerty and this is a huge string\n");
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
-    case REVERT:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("z"));
-      }
-      break;
-    case ZOOMIN:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL(";"));
-      }
-      break;
-    case ZOOMOUT:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL("-"));
-      }
-      break;
-  }
-  return true;
+    switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                print("mode just switched to qwerty and this is a huge string\n");
+                set_single_persistent_default_layer(_QWERTY);
+            }
+            return false;
+            break;
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case ADJUST:
+            if (record->event.pressed) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            return false;
+            break;
+        case REVERT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL("z"));
+            }
+            break;
+        case ZOOMIN:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(";"));
+            }
+            break;
+        case ZOOMOUT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL("-"));
+            }
+            break;
+    }
+    return true;
 }
